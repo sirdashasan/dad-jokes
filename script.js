4e9c212e1c1f7a43705f289bf817cc788b477c52
sirdashasan <sirdashasan@hotmail.com>
2024-02-09 21:14:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// USING ASYNC AWAIT
+async function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    } 
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
+
+    }
+
+
+
+// USING .then()
+/* İlk başta aşağıdaki gibi yaptık sonra yukarıdaki daha temiz bir kullanım olduğu için ona çevirdik. */
+/*
+function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    } 
+
+    fetch('https://icanhazdadjoke.com', config).then(res => res.json()).then(data => {
+        jokeEl.innerHTML = data.joke
+    })
+} */
+
